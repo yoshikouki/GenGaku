f075089f8fc37eda33ed1aa7e9484f75d3fa8036
yoshikouki <yoshikouki@gmail.com>
2024-04-10 09:02:40 +0900

    Update package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import pages from "@hono/vite-cloudflare-pages";
 import devServer from "@hono/vite-dev-server";
-import adapter from "@hono/vite-dev-server/cloudflare";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -10,7 +9,6 @@ export default defineConfig({
   plugins: [
     pages(),
     devServer({
-      adapter,
       entry: "src/index.tsx",
     }),
   ],
